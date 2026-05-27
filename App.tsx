3c22024106e1e508d6e0eaf1a67f1e020df442af
Temrjan <x.temrjan@gmail.com>
2026-05-27 17:36:29 +0500

    feat: initial scaffold

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+export default function App(): React.JSX.Element {
+  return (
+    <SafeAreaView className="flex-1 bg-white">
+      <Text className="text-center text-black mt-20 text-xl">
+        Rustok Mobile
+      </Text>
+    </SafeAreaView>
+  );
+}
